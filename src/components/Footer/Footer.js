31324f81af7f9331d5c6713fb3bd94700d929c58
Kartikayyy <kartikaygupta2808@gmail.com>
2024-08-06 12:08:10 +0530

    Inital commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./footer.scss"
+const Footer = () => {
+  return (
+		<div className="footer">
+			<div>CineVault</div>
+			<div>©2024, CineVault and its affiliates</div>
+		</div>
+	);
+}
+
+export default Footer
---
+++ src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Pnf from "../../images/pnf.jpg";
+import "./pagenotfound.scss"
+const PageNotFound = () => {
+  return (
+		<div class="container">
+			<img src={Pnf} className="photo" alt=''></img>
+		</div>
+	);
+}
+
+export default PageNotFound
